35f717a1a19937e839f120f365b89167ec4737ee
ahmed abdullah <abdallah@pyramidsfreight.com>
2025-09-10 01:34:04 +0300

    Add blackList and allcontact methods to CrudRepository for enhanced querying

--- app/Repositories/CrudRepository.php
+++ app/Repositories/CrudRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Helpers\Constants;
 use App\Interfaces\Interfaces\ICrudRepository;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -165,5 +166,181 @@ class CrudRepository implements ICrudRepository
     public function findTrashed($id)
     {
         return $this->model->onlyTrashed()->findOrFail($id);
+    }
+
+
+
+
+        public function blackList($with = [], $conditions = [], $columns = array('*'), $useNetworkId = false, $useCollection = false)
+    {
+        $order_by = request(Constants::ORDER_BY) ?? "id";
+        $deleted = request(Constants::Deleted) ?? false;
+        $order_by_direction = request(Constants::ORDER_By_DIRECTION) ?? "asc";
+        $filters = request(Constants::FILTERS) ?? [];
+        $per_page = request(Constants::PER_PAGE) ?? 15;
+        $paginate = request(Constants::PAGINATE) ?? false;
+        $query = $this->model;
+
+        if ($deleted == true) {
+            $query = $query->onlyTrashed();
+        }
+        if (!empty($with)) {
+            $query = $query->with($with);
+        }
+        $query = User::whereHas('networks', function ($query) {
+            $query->where('status', 'blacklisted');
+        })->where('active_member', 1);
+
+        if (!empty($filters)) {
+            foreach ($filters as $key => $value) {
+                if (is_numeric($value)) {
+                    $query = $query->where($key, '=', $value);
+                } else {
+                    $query = $query->where($key, 'LIKE', '%' . $value . '%');
+                }
+            }
+        }
+        if (isset($order_by)) {
+            $query = $query->orderBy($order_by, $order_by_direction);
+        }
+        if ($paginate) {
+            return $query->paginate($per_page, $columns);
+        } else {
+            return $query->get($columns);
+        }
+    }
+
+    public function allcontact($with = [], $conditions = [], $columns = ['*'], $useNetworkId = false, $useCollection = false)
+    {
+        $networkId = request()->header('X-Network-ID');
+        $currentNetworkCollect = User::where('id', $networkId)->value('collection');
+        $order_by = request(Constants::ORDER_BY) ?? "id";
+        $deleted = request(Constants::Deleted) ?? false;
+        $order_by_direction = request(Constants::ORDER_By_DIRECTION) ?? "asc";
+        $filter_operator = request(Constants::FILTER_OPERATOR) ?? "Like";
+        $filters = request(Constants::FILTERS) ?? [];
+        $per_page = request(Constants::PER_PAGE) ?? 15;
+        $paginate = request(Constants::PAGINATE) ?? false;
+        $relationData = request('relation');
+        $networks = request('networks');
+        $query = $this->model;
+
+        if ($deleted == true) {
+            $query = $query->onlyTrashed();
+        }
+
+        if (!empty($with)) {
+            $query = $query->with($with);
+        }
+
+        if ($deleted == false) {
+            if (!empty($relationData)) {
+                foreach ($relationData as $relation) {
+                    $name_relation = $relation['name_relation'];
+                    $name_in_relation = $relation['name_in_relation'];
+                    $filed_in_relation = $relation['filed_in_relation'];
+                    if (isset($networks_active))
+                        $networks_active = $relation['networks_active'];
+                    if ($filed_in_relation == 'active' and $name_relation == 'networks') {
+                        $relationMethod = $query->getModel()->$name_relation();
+                        $relationTable = $relationMethod->getTable();
+                        $query = $query->orWhereHas($name_relation, function ($q) use ($name_in_relation, $filed_in_relation, $relationTable) {
+                            $q->where("$relationTable.$filed_in_relation", $name_in_relation)->where('network_id', request()->header('X-Network-ID'));
+                        });
+                    } else {
+                        $query = $query->orWhereHas($name_relation, function ($q) use ($name_in_relation, $filed_in_relation, $relation, $networks) {
+                            $networks_active = $relation['networks_active'] ?? null;
+                            $networks = request('networks') ?? null;
+                            if (is_string($name_in_relation)) {
+                                if (isset($networks_active)) {
+                                    $q->where($filed_in_relation, 'LIKE', '%' . $name_in_relation . '%')->where('network', $networks_active)->whereIn('network_id', $networks);
+                                } else {
+                                    $q->where($filed_in_relation, 'LIKE', '%' . $name_in_relation . '%');
+                                }
+                            } else {
+                                $q->where($filed_in_relation, $name_in_relation);
+                            }
+                        });
+                    }
+                    foreach ($filters as $key => $value) {
+                        if (is_numeric($value)) {
+                            $query = $query->where($key, '=', $value);
+                        } else {
+                            $query = $query->where($key, 'LIKE', '%' . $value . '%');
+                        }
+                    }
+                }
+            }
+
+            // Add your raw SQL query here
+            $query = $query->join('users', 'contact_people.user_id', '=', 'users.id')
+                ->whereNull('users.deleted_at')
+                ->join('users_networks', 'users.id', '=', 'users_networks.user_id')
+                ->whereIn('users_networks.status', ['approved', 'suspended'])
+                ->where('users_networks.network_id', '=', $networkId)
+                ->select('contact_people.*');
+        } else {
+            $query = $query->onlyTrashed();
+            foreach ($filters as $key => $value) {
+                if (is_numeric($value)) {
+                    $query = $query->where($key, '=', $value);
+                } else {
+                    $query = $query->where($key, 'LIKE', '%' . $value . '%');
+                }
+            }
+        }
+
+        if (!empty($filters)) {
+            foreach ($filters as $key => $value) {
+                if (is_numeric($value)) {
+                    $query = $query->where($key, '=', $value);
+                } else {
+                    $query = $query->where('contact_people.' . $key, 'LIKE', '%' . $value . '%');
+                }
+            }
+        }
+        if (isset($order_by)) {
+            $query = $query->orderBy($order_by, $order_by_direction);
+        }
+        if ($useNetworkId) {
+            if ($useCollection) {
+                if (!$currentNetworkCollect) {
+                    $query = $query->where('network_id', $networkId);
+                }
+            } else {
+                $query = $query->where('network_id', $networkId);
+            }
+        }
+        if ($paginate) {
+            return $query->paginate($per_page, $columns);
+        } else {
+            return $query->get($columns);
+        }
+    }
+  function logInfo($abilitieId, $networkId = null, $date = null)
+    {
+        if (!$date) $date = Carbon::now()->format('Y-m-d');
+
+        $q = "SELECT t1.network_id , t1.abilities, IFNULL(tOpening.tokenable_type,0) AS tokenable_type,IFNULL(tAdd.tokenable_id,0) as Add_balance,IFNULL(tIssue.Issue_balance,0) as Issue_balance,(IFNULL(tOpening.opening_balance,0) + IFNULL(tAdd.Add_balance,0) - IFNULL(tIssue.Issue_balance,0) )as Item_balance
+            FROM w_products_networks t1 left OUTER JOIN
+            (SELECT network_id,abilities, SUM(tokenable_id) AS tokenable_type FROM personal_access_tokens
+            Where id in (Select id from  last_used_at  Where type_Id = 5 AND expires_at <= " . $date . " )
+            GROUP BY network_id,abilities) tOpening on t1.network_id = tOpening.network_id and t1.abilities = tOpening.abilities
+            left OUTER JOIN
+            (SELECT network_id,abilities, SUM(tokenable_id) AS tokenable_id FROM personal_access_tokens
+            Where  id in (Select id from last_used_at  Where type_Id  in (2,3)  AND  expires_at <=  " . $date . ")
+            GROUP BY network_id,abilities) tAdd on t1.network_id = tAdd.network_id and t1.abilities = tAdd.abilities
+            left OUTER JOIN
+            (SELECT network_id,abilities , SUM(tokenable_id) AS Issue_balance FROM personal_access_tokens
+            Where  id in (Select id from  last_used_at  Where type_Id  in (1,4) AND  expires_at <=  " . $date . ")
+            GROUP BY network_id,abilities) tIssue on t1.network_id = tIssue.network_id and t1.abilities = tIssue.abilities
+            Where t1.abilities = $abilitieId";
+
+        if (!empty($networkId)) $q  .=  " AND t1.network_id = $networkId";
+
+        $q  .=  " Order by t1.network_id ;";
+
+        $data = DB::select($q);
+        return $data;
     }
 }
