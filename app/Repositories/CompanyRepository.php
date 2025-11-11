da38dd7f4cf08695b5173081a688fed23d4040b0
zeinabYounes <zeinabyounes099@gmail.com>
2025-11-11 13:56:20 +0200

    comment notify user in company

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -77,7 +77,7 @@ class CompanyRepository extends CrudRepository implements CompanyRepositoryInter
       
             $employee ->update($employee_data);
             
-            $employee->notify(new SendPassword($password ,$this->dashboard_type));
+         //   $employee->notify(new SendPassword($password ,$this->dashboard_type));
 
             return ["employee"=>$employee,"password"=>$password] ;
         });
